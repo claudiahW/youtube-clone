86f09ba888e9ba7d6b61c347c32155ed81111dff
claudiahW <murungiclaud@gmail.com>
2024-10-19 21:38:09 +0300

    make video full screen

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Video.css'
+import PlayVideo from '../../Components/PlayVideo/PlayVideo'
 
 const Video = () => {
   return (
-    <div>
+    <div className='play-container'>
 
+<PlayVideo />
 
     </div>
   )
